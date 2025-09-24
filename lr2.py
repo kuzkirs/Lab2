adcd0a789d0e7443cae747bbef82024cfb9ae97b
kuzkirs <kuzyakuzmynskiy@gmail.com>
2025-09-24 09:30:01 +0300

    lr2.py

--- lr2.py
+++ lr2.py
@@ -14,5 +14,24 @@ def print_ascii_codes(*parts):
 print_ascii_codes(
     "Kuzmynskiy", "Kirill",
     "Bulavka", "Oleksandr",
-    "", "",
+    def print_ascii_codes(*parts):
+    full_name = " ".join(parts)
+    for char in full_name:
+        if char == " ":
+            print()  
+            continue
+        print(char)
+        print(f"10: {ord(char)}")
+        print(f"2: {bin(ord(char))[2:].zfill(8)}")
+        print(f"16: {hex(ord(char))[2:].upper()}")
+        print()
+
+# Пример с 4 именами и 4 фамилиями:
+print_ascii_codes(
+    "Kuzmynskiy", "Kirill",
+    "Bulavka", "Oleksandr",
+     "Dmytro", "Yukhymovych",
 )
+
+)
+
