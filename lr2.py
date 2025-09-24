5a91b0cdf0d457bead93fb91167117d49297b72c
kuzkirs <kuzyakuzmynskiy@gmail.com>
2025-09-24 09:09:01 +0300

    Add files via upload

---
+++ lr2.py
@@ -0,0 +1,13 @@
+def print_ascii_codes(name, surname):
+    full_name = name + " " + surname
+    for char in full_name:
+        if char == " ":
+            print()  
+            continue
+        print(char)
+        print(f"10: {ord(char)}")
+        print(f"2: {bin(ord(char))[2:].zfill(8)}")
+        print(f"16: {hex(ord(char))[2:].upper()}")
+        print()
+
+print_ascii_codes("Kuzmynskiy", "Kirill")
